297120e51ab278c885aeff9481332905b4204c0c
Stefan Gatu <stefan.gatu@gmail.com>
2023-08-12 11:09:59 +0100

    feat: data type alias and list data type struct

---
+++ src/commands/implcommands/delete.rs
@@ -0,0 +1,18 @@
+use std::sync::Arc;
+
+use crate::{commands::commands::Command, state::datastate::DataState};
+
+pub struct DeleteCmd {}
+impl DeleteCmd {
+    pub fn execute(data_state: &Arc<DataState>, cmd: &Command) -> Result<Option<Vec<u8>>, String> {
+        if cmd.arguments.len() < 1 {
+            return Err("Command DELETE requires at least 1 parameter".to_owned());
+        } else {
+            for key_b in cmd.arguments.iter() {
+                let str_key = std::str::from_utf8(&key_b).map_err(|_| "Invalid utf8 key.")?;
+                data_state.data.remove(str_key);
+            }
+            return Ok(None);
+        }
+    }
+}
